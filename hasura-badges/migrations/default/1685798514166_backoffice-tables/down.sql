34cb6a99b1f7bbe9067c8ad715760e58b6dca749
Kledi Bushi <55890394+bushikledi@users.noreply.github.com>
2023-07-13 13:28:26 +0000

    made backend fixes

--- hasura-badges/migrations/default/1685798514166_backoffice-tables/down.sql
+++ hasura-badges/migrations/default/1685798514166_backoffice-tables/down.sql
@@ -1,7 +1,10 @@
-DROP VIEW  "engineers";
-DROP VIEW "managers";
-DROP TABLE "requirements_definitions";
-DROP VIEW "engineering_teams";
-DROP TABLE "badges_definitions";
-DROP TABLE "users_relations";
-DROP TABLE "users";
+DROP VIEW IF EXISTS "engineering_teams";
+DROP VIEW IF EXISTS "valid_user_relations";
+DROP VIEW IF EXISTS "managers";
+DROP VIEW IF EXISTS "engineers";
+DROP VIEW IF EXISTS "valid_users";
+
+DROP TABLE IF EXISTS "requirements_definitions";
+DROP TABLE IF EXISTS "badges_definitions";
+DROP TABLE IF EXISTS public.users_relations;
+DROP TABLE IF EXISTS "users";
